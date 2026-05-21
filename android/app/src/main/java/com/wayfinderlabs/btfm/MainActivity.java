5ff50a1cb594e1bf299bbf94c1c2b20abf3e0450
Wayfinder Labs <273322959+WayfinderLabs@users.noreply.github.com>
2026-05-21 22:31:47 +0900

    fix: programmatic window inset handler for Android 15 edge-to-edge

--- android/app/src/main/java/com/wayfinderlabs/btfm/MainActivity.java
+++ android/app/src/main/java/com/wayfinderlabs/btfm/MainActivity.java
@@ -1,5 +1,23 @@
 package com.wayfinderlabs.btfm;
 
+import android.os.Bundle;
+import android.view.View;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowCompat;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+        View rootView = findViewById(android.R.id.content);
+        ViewCompat.setOnApplyWindowInsetsListener(rootView, (v, insets) -> {
+            int top = insets.getInsets(WindowInsetsCompat.Type.statusBars()).top;
+            int bottom = insets.getInsets(WindowInsetsCompat.Type.navigationBars()).bottom;
+            v.setPadding(0, top, 0, bottom);
+            return insets;
+        });
+    }
+}
